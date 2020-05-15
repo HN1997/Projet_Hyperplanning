2b12ba258aafc7c8439dde51eea96d1a1ead7149
AnandMaisuria <63960654+AnandMaisuria@users.noreply.github.com>
2020-05-15 13:38:29 +0200

    Update ConnexionSQL.java

--- src/package_modele/ConnexionSQL.java
+++ src/package_modele/ConnexionSQL.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package connexionsql;
+package package_modele;
 import java.sql.*;
 import java.lang.*;
 import java.util.*;
