dd6032ac9d3e7efdaf0e29682b73a3b764fedaaa
AnandMaisuria <63960654+AnandMaisuria@users.noreply.github.com>
2020-05-23 12:34:14 +0200

    Update EnseignantDAO.java

--- src/package_modele/EnseignantDAO.java
+++ src/package_modele/EnseignantDAO.java
@@ -2,7 +2,7 @@ package package_modele;
 import java.sql.*;
 
 public class EnseignantDAO extends DAO<Enseignant> {
-      
+
     public EnseignantDAO(Connection conn) {
         super(conn);
     }
@@ -21,35 +21,28 @@ public class EnseignantDAO extends DAO<Enseignant> {
     public boolean update(Enseignant obj) {
         return false;
     }
-   
-   @Override
-   public Enseignant find(int id) {
-    Enseignant enseignant = new Enseignant();            
-    try {
-      ResultSet result = this.connect.createStatement(
-        ResultSet.TYPE_SCROLL_INSENSITIVE, 
-        ResultSet.CONCUR_READ_ONLY
-      ).executeQuery(
-        "SELECT * FROM enseignant "+
-        "LEFT JOIN utilisateur ON ID_Utilisateur = enseignant.ID_Utilisateur " + 
-        "AND ID_Utilisateur = "+ id 
-      );
-      if(result.first()){
-        enseignant = new Enseignant(id, result.getString("ID_Cours"));
-        result.beforeFirst();
-        UtilisateurDAO matDao = new UtilisateurDAO(this.connect);
-            
-        while(result.next())
-          enseignant.addUtilisateur(matDao.find(result.getInt("ID_Utilisateur")));
-      }
-    } catch (SQLException e) {
+    
+    @Override
+    public Enseignant find(int id) {
+        Enseignant cours = new Enseignant();
+
+        try {
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM enseignant WHERE ID_Utilisateur= "+id);
+            if (result.first()) {
+                cours = new Enseignant(
+                        id,
+                        result.getInt("ID_Cours"));
+            }
+        } catch (SQLException e) {
+        }
+        return cours;
     }
-    return enseignant;
-  }
 
     @Override
-    public Enseignant find(String Email, String Password) {
+    public Enseignant find(String Email, String Passwd) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
+  
